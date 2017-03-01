474f885f519222406caa6fa382e9dcc79ffe25da
somethign <yeah.right@legitmail.tld>
2017-03-01 21:51:42 +0100

    Reworked threading. Added rate throttling.

--- dns-probe.py
+++ dns-probe.py
@@ -4,6 +4,8 @@ import threading
 import argparse
 import sys
 
+import task_pool
+
 def execute(args):
     '''
     Performs dns probes on the specified domain. 
@@ -19,6 +21,8 @@ def execute(args):
     store_miss  Bool    Store misses as well                False
     verbose     Bool    Log verbose status message to outp  False
     debug       Bool    Log debug messages                  False
+    rate        int     Maximum requests per second         1000
+    threads     int     Number of threads to utilize        1
     '''
     if args["domains"] == []:
         raise Exception("Domains required.")
@@ -48,159 +52,89 @@ def execute(args):
         log(debug, "[+] Using wordlist: %s" % args["wordlist"])
         with open(args["wordlist"], "r") as f:
             wordlist = map(lambda x: x.strip(), f.readlines())
+
     log(debug, "[+] Running with %d threads." % args["threads"])
 
     # Check the specified domains
-    results = []
+    # Prepare threadpool with tasks
+    domains = args["domains"]
     recordtypes = args["types"]
-    for domain in args["domains"]:
-        for recordtype in recordtypes:
-            subresult = lookup(resolver, domain, recordtype)
-            if subresult:
-                answers = []
-                for answer in subresult:
-                    answers.append(str(answer))
+    threadcount = args["threads"]
+    rate = args["rate"]
 
-                    
-                log(verbose, "[+] Hit: %s - %s" % (domain, recordtype))
-                dom_results = {}
-                dom_results["domain"] = domain
-                dom_results["result"] = answers
-                dom_results["type"] = recordtype
-                results.append(dom_results)
+    pool = task_pool.TaskPool(rate, threadcount)
+    for domain in domains:
+        for record_type in recordtypes:
+            pool.add_task(dns_request, resolver, domain, record_type, log, verbose, debug)
 
-                # Add "." to end of domain as this is expected format 
-                if subresult.canonical_name.to_text() != domain + ".": 
-                    # A CNAME record was returned as well, add this.
-                    cname_record = {}
-                    cname_record["domain"] = domain
-                    cname_record["result"] = [ subresult.canonical_name.to_text() ]
-                    cname_record["type"] = "CNAME"
-                    results.append(cname_record)
-                    log(verbose, "[+] Hit CNAME: %s - %s" % (domain, cname_record["result"]))
-            else:
-                log(debug, "[-] Miss: %s - %s" % (domain, recordtype))
-                if args["store_miss"]:
-                    res = {}
-                    res["domain"] = domain
-                    res["result"] = []
-                    res["type"] = "NXDOMAIN"
-                    results.append(res)
+    if len(wordlist) > 0:
+        for domain in domains:
+            for word in wordlist:
+                for recordtype in recordtypes:
+                    pool.add_task(dns_request, resolver, word + "." + domain, recordtype, log, verbose, debug)
 
-    # If wordlist use => bruteforce all specified domains with wordlist.
-    if wordlist:
-        # Divide the wordlist into shards for the different threads.
-        shard_count = args["threads"]
-        word_count = len(wordlist)
-        words_in_shard = word_count/shard_count
-        shards = []
-        for i in range(0, shard_count):
-            offset = i * words_in_shard
-            shards.append((offset, offset + words_in_shard))
-        # If space is not evenly divided, ensure that the last thread checks the
-        # leftovers
-        shards[len(shards)-1] = (shards[len(shards)-1][0], word_count)
-
-        log_lock = threading.Lock()
-
-        for domain in args["domains"]:
-            threads = []
-            for i in range(0, shard_count):
-                threads.append(ThreadedLookup(resolver, domain, wordlist, args["types"], verbose, debug, args["store_miss"], log_lock, shards[i][0], shards[i][1]))
-            
-            for thread in threads:
-                thread.start()
+    # Perform all dns requests (wait until everything is done)
+    # RAM issues?
+    try:
+        pool.perform_tasks()
+    except KeyboardInterrupt:
+        pool.cleanup()
+        return []
     
-            for thread in threads:
-                try:
-                    thread.join()
-                except:
-                    continue
-
-            for thread in threads:
-                results.extend(thread.results)
+    # Extract the results
+    results = []
+    store_miss = args["store_miss"]
+    for dns_response in list(pool.result_reporter.queue.queue):
+        if dns_response:
+            name = dns_response[0]
+            recordtype = dns_response[1]
+            answers = dns_response[2]
+            result = {}
+            result["domain"] = name
+            result["result"] = map(str, answers)
+            result["type"] = recordtype
+            results.append(result)
+            # Add "." to end of domain as this is expected format 
+            if answers.canonical_name.to_text() != name + ".": 
+                # A CNAME Was returned as well, store it.
+                cname_record = {}
+                cname_record["domain"] = name
+                cname_record["result"] = [ answers.canonical_name.to_text() ]
+                cname_record["type"] = "CNAME"
+                results.append(cname_record)
+        elif store_miss:
+            result = {}
+            result["domain"] = name
+            result["result"] = []
+            result["type"] = "NXDOMAIN"
+            results.append(result)
             
     return results
 
-class ThreadedLookup(threading.Thread):
-    def __init__(self, resolver, domain, wordlist, recordtypes, verbose, debug, store_miss, log_lock, start_idx, stop_idx):
-        threading.Thread.__init__(self)
-        self.log_lock = log_lock
-        self.resolver = resolver
-        self.wordlist = wordlist
-        self.recordtypes = recordtypes
-        self.domain = domain
-        self.verbose = verbose
-        self.debug = debug
-        self.start_index = start_idx
-        self.stop_index = stop_idx
-        self.store_miss = store_miss
-        self.results = []
-
-    def run(self):
-        for i in range(self.start_index, self.stop_index):
-            subdomain = "%s.%s" % (self.wordlist[i], self.domain)
-            for recordtype in self.recordtypes:
-                subresult = lookup(self.resolver, subdomain, recordtype)
-                if subresult:
-                    answers = []
-                    for answer in subresult:
-                        answers.append(str(answer))
-                    
-                    self.log_lock.acquire()             
-                    log(self.verbose, "[+] Hit: %s - %s" % (subdomain, recordtype))
-                    self.log_lock.release()
-        
-                    dom_results = {}
-                    dom_results["domain"] = subdomain
-                    dom_results["result"] = answers
-                    dom_results["type"] = recordtype
-                    self.results.append(dom_results)
-                    # Add "." to end of domain as this is expected format 
-                    if subresult.canonical_name.to_text() != subdomain + ".":
-                        # A CNAME record was returned as well, add this.
-                        cname_record = {}
-                        cname_record["domain"] = subdomain
-                        cname_record["result"] = [ subresult.canonical_name.to_text() ]
-                        cname_record["type"] = "CNAME"
-                        self.log_lock.acquire()
-                        log(self.verbose, "[+] Hit CNAME: %s - %s" % (cname_record["domain"], cname_record["result"]))
-                        self.log_lock.release()
-                        self.results.append(cname_record)
-                else:
-                    self.log_lock.acquire()             
-                    log(self.debug, "[-] Miss: %s - %s" % (subdomain, recordtype))
-                    self.log_lock.release()
-                    
-                    if self.store_miss:
-                        res = {}
-                        res["domain"] = subdomain
-                        res["result"] = []
-                        res["type"] = "NXDOMAIN"
-                        self.results.append(res)
-                    
-            
-         
-
-def lookup(resolver, domain, recordtype):
+def dns_request(resolver, name, recordtype, logger, verbose, debug):
     answers = None
     try:
-        answers = resolver.query(domain, recordtype)
+        answers = resolver.query(name, recordtype)
+        logger(verbose, "[+] Hit %s (%s)" % (name, recordtype))
     except Exception as e:
+        logger(debug, "[-] Miss %s (%s)" % (name, recordtype))
         return None
-    return answers
-        
+    return (name, recordtype, answers)
 
+
+log_lock = threading.Lock()
 def log(should_log, msg):
+    global log_lock
+    log_lock.acquire()
     if should_log:
         print(msg)
+    log_lock.release()
 
 def parse_cmdline():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--domains", help="The domains to probe.", nargs="*", default=[])
     parser.add_argument("-n", "--nameservers", help="The domain servers to use", nargs="*", default=[])
     parser.add_argument("-l", "--wordlist", help="Wordlist of possible subdomains.", default="")
-    parser.add_argument("-T", "--threads", help="Number of threads to use when bruting with a wordlist.", default=1, type=int)
     parser.add_argument("-t", "--types", help="The type of dns record to look up.", default=["A"], choices=["A", "AAAA", "MX", "TXT", "NS", "SOA", "CNAME"], nargs="*")
     parser.add_argument("-v", "--verbose", help="Verbose status messages.", default=False, action="store_true")
     parser.add_argument("-f", "--format", help="Format of the output.", default="grep", choices={"grep", "json"})
@@ -208,6 +142,8 @@ def parse_cmdline():
     parser.add_argument("-r", "--read", help="Read domains from file.")
     parser.add_argument("-w", "--write", help="Write results to files using the given base path.", default="")
     parser.add_argument("-s", "--store-miss", help="Store the result even tho there was a NXDOMAIN (miss) when performing the lookup.", default=False, action="store_true")
+    parser.add_argument("-T", "--threads", help="Number of threads to use when bruting with a wordlist.", default=1, type=int)
+    parser.add_argument("-R", "--rate", help="Maximum number of requests per second allowed", default=1000, type=int)
     
     args = parser.parse_args() 
 
